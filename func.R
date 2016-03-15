48a91da2b78904976bb0a75d869ed61e89069f02
EinarP <einarpo@gmail.com>
2016-03-15 18:20:10 +0000

    sequence design complete

--- func.R
+++ func.R
@@ -4,48 +4,67 @@ library(igraph)
 
 #construction
 asq <- function(x) {
-    
-    m <- list(name=x)
-    
-    t <- data.frame(id=1, parent=NA, cmd='init', timeline=NA,
-        stringsAsFactors=FALSE)
-    
-    s <- list(graph.empty())
-    
-    structure(list(meta=m, trace=t, struct=s), class='asq')
+  
+  m <- list(name=x, outp='plot')
+  
+  t <- data.frame(id=0, parent=NA, cmd='init', timeline=NA,
+    stringsAsFactors=FALSE)
+  
+  s <- list(graph.empty())
+  
+  structure(list(meta=m, trace=t, struct=s), class='asq')
 }
 
 #print method
 print.asq <- function(x) {
-    
-    cat(x$meta$name, '\n\n')
-    print(tail(x$trace, 3))
-    
-    if (vcount(x$struct[[1]]) > 0) tkplot(x$struct[[1]])
+  
+  cat(x$meta$name, '\n\n')
+  print(tail(x$trace, 3))
+  
+  if(vcount(x$struct[[1]]) > 0) {
+    if(x$meta$outp=='plot')
+      plot(x$struct[[1]], edge.arrow.size=0.3, vertex.frame.color=NA,
+           vertex.label.family='sans')   
+    else
+      tkplot(x$struct[[1]])
+  }
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #CENTER transformations
 
-addCenter <- function(ang, center) {
+addCenter <- function(analseq, center) {
+  
+  ang <- analseq$struct[[1]]
   
   if (!is.element(center, vertex.attributes(ang)$name)) {
-    ang <- ang + vertices(center)
+    ang <- ang + vertices(center, shape='rectangle')
   }
   
   linkList <- obs[grep(paste0(center, '>'), obs$object), 'value']
   for (i in 1:length(linkList)) {
     if (!is.element(linkList[i], vertex.attributes(ang)$name)) {
-      ang <- ang + vertices(linkList[i])
+      ang <- ang + vertices(linkList[i], shape='rectangle')
     }
-    ang <- ang + edge(center, linkList[i])
+    ang <- ang + edge(center, linkList[i], edge.label='loll')
   }
   
-  ang
+  analseq$trace <- rbind(analseq$trace, c(1, 0, 'addCenter', NA))
+  analseq$struct[[1]] <- ang
+  
+  analseq
 }
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #VOID transformations
 
-voidCenter <- function(ang, center) {
-  delete.vertices(ang, center)
+voidCenter <- function(analseq, center) {
+  
+  ang <- analseq$struct[[1]]
+  
+  ang <- delete.vertices(ang, center)
+  
+  analseq$trace <- rbind(analseq$trace, c(1, 0, 'voidCenter', NA))
+  analseq$struct[[1]] <- ang
+  
+  analseq
 }
