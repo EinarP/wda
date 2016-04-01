d5d29113762ef9df11475c29f7bfd987f024e00a
EinarP <einarpo@gmail.com>
2016-04-01 17:29:45 +0300

    bottom-up linking

--- democenter.R
+++ democenter.R
@@ -5,18 +5,34 @@ par(mfrow = c(1,1), cex.lab=0.8)
 source('func.R')
 
 #demo data
-obs <- data.frame(dtstamp='20160321', object='C1',
-  property='field', value='id', stringsAsFactors=FALSE)
+obs <- data.frame(object=character(), property=character(),
+  value=character(), dtstamp=character(), stringsAsFactors=FALSE)
 
-obs <- rbind(obs, c('20160321', 'C2', 'field', 'id'))
-obs <- rbind(obs, c('20160321', 'C3', 'field', 'id'))
-obs <- rbind(obs, c('20160321', 'C4', 'field', 'id'))
-obs <- rbind(obs, c('20160321', 'C5', 'field', 'id'))
 
-obs <- rbind(obs, c('20160321', 'C1>id', 'linkdef', 'C2'))
-obs <- rbind(obs, c('20160321', 'C1>id', 'linkdef', 'C3'))
-obs <- rbind(obs, c('20160321', 'C1>id', 'linkdef', 'C4'))
-obs <- rbind(obs, c('20160321', 'C5>id', 'linkdef', 'C1'))
+# obs <- data.frame(dtstamp='20160321', object='C1',
+#  property='field', value='id', stringsAsFactors=FALSE)
+# obs <- rbind(obs, c('20160321', 'C2', 'field', 'id'))
+# obs <- rbind(obs, c('20160321', 'C3', 'field', 'id'))
+# obs <- rbind(obs, c('20160321', 'C4', 'field', 'id'))
+# obs <- rbind(obs, c('20160321', 'C5', 'field', 'id'))
+# obs <- rbind(obs, c('20160321', 'C1>id', 'linkdef', 'C2'))
+# obs <- rbind(obs, c('20160321', 'C1>id', 'linkdef', 'C3'))
+# obs <- rbind(obs, c('20160321', 'C1>id', 'linkdef', 'C4'))
+# obs <- rbind(obs, c('20160321', 'C5>id', 'linkdef', 'C1'))
+
+obs <- data.frame(object=character(), property=character(),
+  value=character(), dtstamp=character(), stringsAsFactors=FALSE)
+
+obs[1, ] <- c('C1>id','linkdef','C2','')
+obs[2, ] <- c('C1>id','linkdef','C3','')
+obs[3, ] <- c('C4>id','linkdef','C1','')
+obs[4, ] <- c('C2>id','linkdef','C5','')
+obs[5, ] <- c('C6>id','linkdef','C2','')
+obs[6, ] <- c('C3>id','linkdef','C7','')
+obs[7, ] <- c('C8>id','linkdef','C3','')
+obs[8, ] <- c('C9>id','linkdef','C3','')
+obs[9, ] <- c('C10>id','linkdef','C7','')
+obs[10, ] <- c('C7>id','linkdef','C2','')
 
 obs
 
@@ -24,7 +40,7 @@ obs
 trdemo <- asq('Transformations demonstration')
 
 #Add centers
-trdemo <- addCenter(trdemo, c('C1','C5'), level=1)
+trdemo <- addCenter(trdemo, 'C1', depth=2)
 trdemo
 
 #List current centers
--- func.R
+++ func.R
@@ -11,10 +11,9 @@ asq <- function(x) {
   s <- data.frame(id=1, parent=NA, transformation=paste0("asq('", x, "')"),
     checkpoint=NA, stringsAsFactors=FALSE)
 
-#TODO: Seems better to store vertices, edges, communities as data frame
+#TODO: Perhaps store vertices, edges, communities as data frame
   g <- list(graph.empty())
 
-#structure(list(meta=m, trace=t, struct=s), class='asq')
   structure(list(name=x, seed=1, output='plot', seq=s, struct=g), class='asq')
 }
 
@@ -24,21 +23,19 @@ asq <- function(x) {
 print.asq <- function(x, seed=NA) {
 
 #Set random number generator
-  if (is.na(seed)) {
-    seed <- x$seed
-  }
-  set.seed(seed)
+  set.seed(ifelse(is.na(seed), x$seed, seed))
 
 #Excerpt of trace table
   cat(x$name, '\n\n')
-  print(tail(x$seq[ ,2:ncol(x$seq)], 3))
+  print(tail(x$seq[ ,3:ncol(x$seq)], 3))
 
 #Plot on requested device
   curg <- tail(x$struct, 1)[[1]]
   if(vcount(curg) > 0) {
     if(x$output=='plot') {
       plot(curg, xlab=tail(x$seq, 1)$transformation,
-        edge.arrow.size=0.3, vertex.frame.color=NA, vertex.label.family='sans')   
+        edge.arrow.size=0.3, vertex.frame.color=NA, vertex.label.family='sans',
+        edge.label.cex=0.8)   
     } else {
       tkplot(curg)
     }
@@ -59,7 +56,7 @@ addAnalysisStep <- function(aseq, cmd, ang, chkp=NA) {
   
   aseq$seq <- rbind(aseq$seq, newtr)
 
-#TODO: Likely need to save every graph and also clusters separately 
+#TODO: Perhaps need to save graphs and clusters separately 
   aseq$struct[[length(aseq$struct)+1]] <- ang
   
   aseq
@@ -70,38 +67,47 @@ addAnalysisStep <- function(aseq, cmd, ang, chkp=NA) {
 ################################################################################
 
 # Add centers
-addCenter <- function(aseq, center, level=0, chkp=NA, req=NA) {
+addCenter <- function(aseq, center, depth=1, chkp=NA) {
   
-  ang <- aseq$struct[[length(aseq$struct)]]
+  ang <<- aseq$struct[[length(aseq$struct)]]
   
-  for (i in 1:length(center)) {
-    
-    if (!is.element(center[i], vertex.attributes(ang)$name)) {
-      ang <- ang + vertices(center[i], shape = 'rectangle')
-    }
-    
-    if (level > 0) {
-      lcenter <- obs[grep(paste0(center[i], '>'), obs$object), 'value']
-      
-      for (j in 1:length(lcenter)) {
-        
-        if (!is.element(lcenter[j], vertex.attributes(ang)$name)) {
-          laseq <- aseq
-          laseq$struct[[length(aseq$struct)]] <- ang
-          laseq <- addCenter(laseq, lcenter[j], level-1, req=TRUE)
-          ang <- laseq$struct[[length(aseq$struct)]]
-        }
-        
-        ang <- ang + edge(center[i], lcenter[j])
-      }
-    }
+  sapply(center, function(x) addNeighbor(x, depth))
+
+  addAnalysisStep(aseq, match.call(), ang, chkp)
+}
+
+# Add neighbors when adding centers
+addNeighbor <- function(center, depth) {
+
+  # add head center if not present
+  if (!is.element(center, vertex.attributes(ang)$name)) {
+    ang <<- ang + vertices(center, shape = 'rectangle')
   }
+
+  if (depth > 0) {  
   
-  if (is.na(req)) {
-    addAnalysisStep(aseq, match.call(), ang, chkp)
-  } else {
-    aseq$struct[[length(aseq$struct)]] <- ang
-    aseq
+    # top-down linking of tails
+    tailc <- obs[grep(paste0(center, '>'), obs$object), ]
+    linkspec <- paste(tailc$object, tailc$value, sep='>')
+    
+    # bottom-up linking of tails
+    tailc <- obs[obs$value==center,c('object','value')]
+    linkspec <- c(linkspec, paste(tailc$object, tailc$value, sep='>'))
+
+    # add tail center and link head to tail
+    sapply(linkspec, function(curlink) {
+      
+      newhead <- unlist(strsplit(curlink, '>', fixed=TRUE))[1]
+      newtail <- unlist(strsplit(curlink, '>', fixed=TRUE))[3]
+      addNeighbor(ifelse(center==newhead, newtail, newhead), depth-1)
+      
+      ange <- get.edgelist(ang, names=T)
+      anglink <- paste(ange[,1], E(ang)$label, ange[,2], sep='>')
+      if (!is.element(curlink, anglink)) {
+        newlink <- unlist(strsplit(curlink, '>', fixed=TRUE))[2]
+        ang <<- ang + edge(newhead, newtail, label=newlink)
+      }
+    })
   }
 }
 
@@ -110,7 +116,7 @@ addCenter <- function(aseq, center, level=0, chkp=NA, req=NA) {
 # Retrieve all center references
 getCenter <- function(aseq) {
 
-  ang <- aseq$struct[[1]]
+  ang <- aseq$struct[[length(aseq$struct)]]
   
   vertex.attributes(ang)$name  
 }
@@ -122,7 +128,7 @@ getCenter <- function(aseq) {
 # Add boundary
 addBoundary <- function(aseq, clustering, tline=NA) {
   
-  trp <- aseq$struct[[1]]
+  trp <- aseq$struct[[length(aseq$struct)]]
   tc <- make_clusters(trp, clustering)
   plot(tc, trp)
   
@@ -142,7 +148,7 @@ getBoundary <- function(x) {
 # Remove centers
 voidCenter <- function(aseq, center, tline=NA) {
   
-  ang <- aseq$struct[[1]]
+  ang <- aseq$struct[[length(aseq$struct)]]
   
   ang <- delete.vertices(ang, center)
   
