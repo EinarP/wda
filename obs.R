4d9c2053f458f1301b678b721c26701d85401ab9
EinarP <einarpo@gmail.com>
2019-01-30 09:40:42 +0200

    opvc

--- obs.R
+++ obs.R
@@ -4,6 +4,8 @@
 
 obs <- function(x, ...) UseMethod('obs')
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 obs.character <- function(obs) {
 # obs = {E1>a|E2, E1|E2, E1|E2=v, E>a, E>a=v}; TODO: E=id>a=v
   
@@ -19,12 +21,12 @@ obs.character <- function(obs) {
     
     # Default missing object, property, or value
     if (is.na(p)) {
-      if (grepl('|', o)) {
+      if (grepl('\\|', o)) {
         s <- strsplit(op, '\\|')[[1]]
         o <- paste0(s[1], '>id|', s[2]) ; p <- 'type'
         if (is.na(v)) v <- 'weak'
       } else {
-        p <- 'id'
+ #       p <- 'id'
       }
     }
    
@@ -32,24 +34,42 @@ obs.character <- function(obs) {
     c(object = o, property = p, value = v)
   }))
   
-  splitObs(data.frame(obs_mtx, stringsAsFactors = FALSE))
+  completeObsFields(data.frame(obs_mtx, stringsAsFactors = FALSE))
 }
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# TODO: Perhaps ouput E, E2, A, MA, V, ID, CKPT in obs already?
-splitObs <- function(obs) {
-
-  s <- strsplit(obs$object, '\\|')
-  obs$h_ent_src <- unlist(lapply(s, function(x) x[1]))
-  obs$h_ent_dest <- lapply(s, function(x) x[2])
-
-  s <- strsplit(obs$h_ent_src, '>')
-  obs$h_ent_src <- lapply(s, function(x) x[1])
-  obs$h_attr <- ifelse(is.na(obs$h_ent_dest), obs$property, lapply(s, function(x) x[2]))
-  obs$h_meta_attr <- ifelse(is.na(obs$h_ent_dest), NA, obs$property) 
-
-  obs$h_value <- obs$value
+obs.data.frame <- function(opvci) {
   
-  obs
+  completeObsFields(opvci)
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# TODO: Perhaps ouput E, E2, A, MA, V, ID, CKPT in obs already?
+completeObsFields <- function(opvci) {
+
+  # Add missing or tidy up existing checkpoints
+  if (!is.element('checkpoint', colnames(opvci))) {
+    opvci$checkpoint <- 'all'
+  } else {
+    opvci[is.na(opvci$checkpoint),'checkpoint'] <- 'all'
+  }
+
+  # Add id for instance modelling
+  if (!is.element('id', colnames(opvci))) {
+    opvci$id <- NA
+  }
+    
+  # Link between entities if h_ent2 present
+  s <- strsplit(opvci$object, '\\|')
+  opvci$h_ent <- unlist(lapply(s, function(x) x[1]))
+  opvci$h_ent2 <- lapply(s, function(x) x[2])
+
+  s <- strsplit(opvci$h_ent, '>')
+  opvci$h_ent <- lapply(s, function(x) x[1])
+  opvci$h_attr <- ifelse(is.na(opvci$h_ent2), opvci$property, lapply(s, function(x) x[2]))
+  opvci$h_meta <- ifelse(is.na(opvci$h_ent2), NA, opvci$property) 
+
+  opvci
 }
